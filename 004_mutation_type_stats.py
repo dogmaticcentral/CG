04aa16bd4aa4cccdfda6233f8ca07d1a23a92e85
Ivana Mihalek <ivana.mihalek@gmail.com>
2015-02-07 15:03:20 +0100

    trawling the TCGA http site

--- 004_mutation_type_stats.py
+++ 004_mutation_type_stats.py
@@ -13,38 +13,43 @@ def main():
     db_name  = 'COAD'
     table = 'somatic_mutations'
 
-    switch_to_db (cursor, db_name)
-
     # unbuffered output
     sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
 
-    ############################
-    print db_name, table, "number of entries:"
-    qry = "select count(1) from " + table
-    rows = search_db(cursor, qry)
-    print "\t", rows[0][0]
-    print 
-    ############################
+    db_names  = ["ACC", "BLCA", "BRCA", "CESC", "CHOL", "COAD", "ESCA", "GBM", "HNSC", "KICH", "KIRC", "KIRP", 
+                 "LAML", "LGG", "LIHC", "LUAD", "LUSC", "OV", "PAAD", "PCPG", "PRAD", "REA", # READ is reseved word
+                 "SKCM", "STAD", "THCA", "UCEC", "UCS", "UVM"]
 
-
-
-    ############################
-    print "variant classification:"
-    qry = "select distinct(variant_classification) from somatic_mutations"
-    rows = search_db(cursor, qry)
-    variants = [row[0] for row in  rows]
-    for variant in variants:
-        qry = "select count(1) from somatic_mutations where variant_classification='%s'" % variant
+    # the order in which we want the variants output:
+    variant_order = ["Missense_Mutation", "Silent", "Nonsense_Mutation", "RNA", "Splice_Site", "Frame_Shift_Ins", 
+                     "Frame_Shift_Del", "In_Frame_Del", "In_Frame_Ins", "Translation_Start_Site", "Nonstop_Mutation", 
+                     "5UTR", "3UTR", "IGR", "Intron", "5Flank"]
+    
+    for db_name in db_names:
+        ############################
+        switch_to_db (cursor, db_name)
+        print 
+        print "################################"
+        print db_name, table
+  
+        qry = "select count(1) from " + table
         rows = search_db(cursor, qry)
-        print "\t", variant, rows[0][0], "cases"
+        total = int (rows[0][0])
+        print "number of entries:", total
+        ############################
 
-    ############################
-    print "variant subtype:"
-    for variant in ['SNP', 'DNP', 'TNP', 'ONP', 'INS', 'DEL', 'conosolidated']:
-        qry = "select count(1) from somatic_mutations where variant_type='%s'" % variant
+
+
+        ############################
+        print "variant classification:"
+        qry = "select distinct(variant_classification) from somatic_mutations"
         rows = search_db(cursor, qry)
-        print "\t", variant, rows[0][0], "cases"
-       
+        variants = [row[0] for row in  rows]
+        for variant in variant_order:
+            if not variant in variants: continue
+            qry = "select count(1) from somatic_mutations where variant_classification='%s'" % variant
+            rows = search_db(cursor, qry)
+            print "\t %30s    %6d cases   (%4.1f%%)" %  (variant, rows[0][0], float(rows[0][0])/total*100 )
 
 
     cursor.close()
---
+++ 200_tcga_https_trawler.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python
+#
+#  requests lib http://docs.python-requests.org/en/latest/
+#
+# html parser: http://www.crummy.com/software/BeautifulSoup/, 
+# see also http://stackoverflow.com/questions/11709079/parsing-html-python
+#
+# downloading from http to a file (using urllib):
+# http://stackoverflow.com/questions/19602931/basic-http-file-downloading-and-saving-to-disk-in-python
+#
+import requests
+from BeautifulSoup import BeautifulSoup
+import urllib
+# Set security warning to always go off by default.
+import warnings
+
+def get_page_text (httpaddr):
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        http_req  = requests.get(httpaddr)
+    return http_req.text
+
+
+not_intersting = ['bio', 'pathology_reports', 'diagnostic_images', 'tissue_images',
+                  'mirnaseq', 'miRNASeq', 'mirna', 
+                  'methylation', 'protein_exp', 'mutations', 'mutations_protected', 
+                  'rnaseqv2', 'cdna', 'cna', 'bisulfiteseq', 'rnaseq', 'totalrnaseqv2',
+                  'transcriptome', 'microsat_i', 'exon', 'tracerel']
+
+#########################################
+def recursive_descent (name_pieces):
+
+
+    tcga_http = "".join(name_pieces)
+    ret_text = get_page_text(tcga_http)
+
+    soup = BeautifulSoup(ret_text)
+    #print dir(soup)
+    for link in soup.findAll('a'):
+        if 'Parent Directory' in link.getText(): continue
+        new_name_piece = link.get('href')
+        if 'http' in  new_name_piece: continue
+        if new_name_piece[-1] != '/': 
+            if 'tar.gz' in new_name_piece[-6:] and not 'mage-tab' in new_name_piece:
+                print "".join(name_pieces[1:]), ":  ", 
+                print new_name_piece
+                # here comes the downloading part
+                # dwnldfile = urllib.URLopener()
+                # dwnldfile.retrieve(tcga_http + new_name_piece, $local_dir + "/" + new_name_piece)
+            continue
+
+        # here we are picking the type of data we are looking for
+        # the whole databse is such a mess I do not know how to look for
+        # the data type I need in a more robust way
+        if new_name_piece[:-1] in not_intersting: continue
+            
+        name_pieces.append(new_name_piece)
+        recursive_descent (name_pieces)
+        name_pieces.pop()
+
+    return
+
+#########################################
+def main():
+    
+    name_pieces = ['https://tcga-data.nci.nih.gov/tcgafiles/ftp_auth/distro_ftpusers/anonymous/tumor/']
+    recursive_descent (name_pieces)
+    
+
+#########################################
+if __name__ == '__main__':
+    main()
