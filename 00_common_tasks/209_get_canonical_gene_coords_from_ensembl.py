89715af026aa37c7ebee240da6ecb3a156cb954e
Ivana Mihalek <ivana.mihalek@gmail.com>
2016-06-02 15:46:37 -0400

    canonical from Ensembl

--- 00_common_tasks/209_get_canonical_gene_coords_from_ensembl.py
+++ 00_common_tasks/209_get_canonical_gene_coords_from_ensembl.py
@@ -55,12 +55,67 @@ def main():
         exit(1)
     cursor = db.cursor()
     latest = get_latest_ensembl_db_for_assembly(cursor)
+
+    chromosomes = [str(x) for x in range(1,23)] + ['X', 'Y']
+    
     for assembly, human_db in latest.iteritems():
         print "latest db for %s is %s" % (assembly, human_db)
         switch_to_db(cursor,  human_db)
-        # gene:  gene_id, canonical_transcript_id
+        # first, lets find the chromosomes
+        qry = "select * from  coord_system where name= 'chromosome' and attrib='default_version'"
+        rows = search_db(cursor, qry)
+        if not rows or len(rows)>1:
+            print "something went wrong with ", qry
+            exit(1)
+              
+        [coord_system_id, species_id, name, version, rank, attrib] =  rows[0]
+        print "\t", coord_system_id, species_id, name, version, rank, attrib
+        if species_id != 1 or version!=assembly:
+            print "oink ?!"
+            exit(1)
+
+        qry = "select *  from seq_region where coord_system_id=%d" % coord_system_id
+        rows = search_db(cursor, qry)
+        region_id = {}
+        for row in rows:
+            [seq_region_id, name, coord_system_id, length] = row
+            if name in chromosomes:
+                region_id[name] = seq_region_id
+        
+
+        for chrom in chromosomes:
+            
+            # gene:  gene_id, canonical_transcript_id
+            # older verions keep stable id in a different table
+            if 'NCBI' in assembly:
+                qry  = "select distinct gene.gene_id,  gene_stable_id.stable_id, gene.canonical_transcript_id from gene, gene_stable_id "
+                qry += "where gene.seq_region_id=%d " % region_id[chrom]
+                qry += "and gene.gene_id = gene_stable_id.gene_id"
+            else:
+                qry = "select distinct gene_id,  stable_id, canonical_transcript_id from gene where seq_region_id=%d" % region_id[chrom]
+            rows = search_db(cursor, qry)
+            print "chrom ", chrom, "number of genes", len(rows)
+            continue
+            gene_id = {}
+            canonical_transcript_id = {}
+            for row in rows:
+                [g, stable_id, c] = row
+                gene_id[stable_id] = g
+                canonical_transcript_id[stable_id] = c
+
+            for stable_id in gene_id.keys():
+                print stable_id
+                qry = "select exon.*, exon_transcript.rank  from exon,  exon_transcript "
+                qry += "where  exon.exon_id = exon_transcript.exon_id "
+                qry += "and exon_transcript.transcript_id = %d " %  canonical_transcript_id[stable_id]
+                print qry
+                rows = search_db(cursor, qry)
+                for row in rows:
+                    print row
+                exit(1)
+                
         # exon_transcript: exon_id, transcript_id, rank
-        # exon: exon_id, seq_region_id, seq_region_start, seq_region_end, seq_region_strand, phaswe, end_phase, is_current
+        # exon: exon_id, seq_region_id, seq_region_start, seq_region_end, seq_region_strand, phase, end_phase, is_current
     cursor.close()
     db.close()
 
