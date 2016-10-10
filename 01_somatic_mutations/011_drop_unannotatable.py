cd1f3f4adca8320cebde84a70a6ab84257952ef3
Ivana <ivana.mihalek@gmail.com>
2016-10-10 15:42:09 -0400

    minor in duplicate resolution

--- 01_somatic_mutations/011_drop_unannotatable.py
+++ 01_somatic_mutations/011_drop_unannotatable.py
@@ -68,14 +68,15 @@ def main():
                 search_db(cursor, qry)
             else:
                 conflicting_id = int(conflict.split(' ')[-1])
-                qry = "select * from %s where id=%d" % (table, conflicting_id)
+                qry = "select conflicts from %s where id=%d" % (table, conflicting_id)
                 rows2 = search_db(cursor, qry)
                 if not rows2:
                     print "the row with id %d does not exist" % conflicting_id
                     qry = "delete from %s where id=%d" % (table, id)
                     search_db(cursor, qry)
                 else:
-                    pass
+                    for row2 in rows2:
+                        print row2
 
         # if conflict is null, then delete, otherwise be a bit more careful
 
