604f688431705764919bf542082661f8fb1513af
Ivana <ivana.mihalek@gmail.com>
2016-10-18 15:33:03 -0400

    metastatic

--- 01_somatic_mutations/014_drop_stuttering_samples.py
+++ 01_somatic_mutations/014_drop_stuttering_samples.py
@@ -37,101 +37,16 @@ def main():
                 "SARC", "SKCM", "STAD", "TGCT", "THCA", "THYM", "UCEC", "UCS", "UVM"]
 
     for db_name in db_names:
-        # check db exists
-        qry = "show databases like '%s'" % db_name
-        rows = search_db(cursor, qry)
-        if not rows:
-            print db_name, "not found"
-            continue
 
-        print " ** ", db_name
         switch_to_db(cursor, db_name)
+        qry  = "select * from mutations_meta where diagnostics like '%stutter%'"
+        rows = search_db(cursor,qry)
+        if not rows: continue
+        print
+        print " ** ", db_name
+        for row in rows:
+            print row
 
-        db_dir = "/mnt/databases/TCGA/" + db_name + "/Somatic_Mutations"
-        if not os.path.isdir(db_dir):
-            print "directory " + db_dir + " not found"
-            exit(1)
-
-        maf_files = []
-        for subdir in os.listdir(db_dir):
-            subfull = "/".join([db_dir, subdir])
-            # we'll not deal with mitochondiral mutations for now
-            mafs = filter(lambda x: x.endswith(".maf") and not 'mitochondria' in x, os.listdir(subfull))
-            if len(mafs) > 1:
-                print "unexpected dir structure:"
-                print subfull
-                print mafs
-                exit(1)
-            if len(mafs) == 0: continue
-            maf_files.append ( "/".join([db_dir, subdir,mafs[0]]) )
-
-        required_fields = get_required_fields()
-        expected_fields = filter (lambda x: x not in ['sample_barcode_short', 'meta_info_index', 'conflict'], get_expected_fields(cursor, db_name, "somatic_mutations"))
-
-        for maffile in maf_files:
-
-            bare_filename = "/".join ( maffile.split('/')[-2:])
-            overall_diagnostics = []
-
-            # first make sure that the file is not empty - in which case
-            # we might have to go and check what's wiht the download
-            if os.path.getsize(maffile) == 0:
-                overall_diagnostics.append(["fail", "file empty"])
-                store_meta_info(cursor, bare_filename, overall_diagnostics)
-                continue
-
-            # check if the file contains  all the info we need and hope to have
-            diagnostics = check_headers(maffile, required_fields, expected_fields)
-            if diagnostics[0] != "pass":
-                overall_diagnostics.append(diagnostics)
-            if diagnostics[0] == "fail":
-                store_meta_info(cursor, bare_filename, overall_diagnostics)
-                continue
-
-            # this one should be pass or fail only
-            assembly_diag = find_reference_genome(cursor, maffile, bare_filename)
-            # if we fail here,  it means it is not clear what assembly was used
-            if assembly_diag[0] == "fail":
-                overall_diagnostics.append(assembly_diag)
-                store_meta_info(cursor, bare_filename, overall_diagnostics)
-                continue
-
-            # might come handy: is this data curated or not?
-            if "automated" in bare_filename.lower():
-                overall_diagnostics.append(["note", "automated"])
-            elif "curated" in bare_filename.lower():
-                overall_diagnostics.append(["note", "curated"])
-
-
-            # I am aware of one way in which the file can be corrupt (stutter), so I am checking for it
-            diagnostics = check_health(maffile)
-            if diagnostics[0] != "pass":
-                overall_diagnostics.append(diagnostics)
-
-            # some seq centers fill in one of the normal alleles as '-' --> actually, it looks like it's Baylor
-            # (all seq centers seem not to bother checking if the other allele is different)
-            diagnostics = check_norm_allele(maffile, 1)
-            if diagnostics[0] != "pass":
-                 overall_diagnostics.append(diagnostics)
-
-            diagnostics = check_norm_allele(maffile, 2)
-            if diagnostics[0] != "pass":
-                 overall_diagnostics.append(diagnostics)
-
-            diagnostics = check_norm_allele(maffile, 0)
-            if diagnostics[0] != "pass":
-                 overall_diagnostics.append(diagnostics)
-
-            diagnostics = check_tumor_alleles(maffile)
-            if diagnostics[0] != "pass":
-                 overall_diagnostics.append(diagnostics)
-
-            overall_diagnostics.append(diagnostics)
-
-            # as a convention, (or convenience) assembly info last
-            overall_diagnostics.append(assembly_diag)
-
-            store_meta_info(cursor, bare_filename, overall_diagnostics)
 
     cursor.close()
     db.close()
