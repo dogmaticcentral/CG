bd419ae71cf3dd7553a33a3c5a957014c994ec3d
Ivana Mihalek <ivana.mihalek@gmail.com>
2018-06-06 15:19:42 +0200

    gnomad

--- icgc/60_clustering_on_the_structure.py
+++ icgc/60_clustering_on_the_structure.py
@@ -57,11 +57,47 @@ def protein_mutations (cursor, tables, gene_symbol):
 	#return list(p53_wt.difference(p53_mut)), list(p53_mut.difference(p53_wt))
 	return list(p53_wt.difference(p53_mut)), list(p53_mut.difference(p53_wt)), list(p53_wt.union(p53_mut))
 
+###############################
+def parse_clust_out(clustering_output):
+	isolated = []
+	clusters = []
 
+	inf = open(clustering_output,"r")
+	reading_isolated = False
+	reading_cluster = False
+	list_of_res = []
+	for line in inf:
+		line = line.strip()
+		if len(line)==0: continue
+		if 'z-score' in line:
+			zscore = float(line.split()[-1])
+			if reading_cluster:
+				clusters.append(list_of_res)
+		elif 'isolated' in line:
+			reading_isolated = True
+			reading_cluster = False
+			list_of_res = []
+			continue
+		elif  'cluster size' in line:
+			if reading_isolated:
+				isolated = list_of_res
+			elif reading_cluster:
+				clusters.append(list_of_res)
+			reading_isolated = False
+			reading_cluster = True
+			list_of_res = []
+			continue
+		elif reading_cluster or reading_isolated:
+			list_of_res.append(line)
 
+	clusters = sorted(clusters, key=lambda cluster: -len(cluster))
+
+	return zscore, isolated, clusters
 
 ###############################
 def main():
+
+
 
 	gene = 'RPL5'
 	pdbf_file       = "/home/ivana/Dropbox/Sinisa/ribosomal/data/structures/5s-rRNP.%s.pdb" % gene
@@ -95,14 +131,53 @@ def main():
 		# clustering run
 		clustering_output  =  "{}.{}.clust.output".format(gene,background)
 		cmd = "{} {} - {} 4.5 > {}".format(clustering_prog, pdbf_file, mut_positions_file, clustering_output)
-		print cmd
 		subprocess.call(["bash","-c", cmd])
 
 		# parse clustering output
+		zscore, isolated, clusters = parse_clust_out(clustering_output)
 
 		# pymol input
+		pymol_file = "{}.{}.clust.pml".format(gene,background)
+		outf = open(pymol_file,"w")
+
+		outf.write("\n")
+		outf.write("load %s\n" %pdbf_file)
+		outf.write("\nbg_color  white\nhide all\nshow cartoon\ncolor white\n")
 
 
+		outf.write("\n")
+		outf.write("#isolated\n")
+		outf.write("select isolated, resi %s\n" % ("+".join(isolated)) )
+		outf.write("show spheres, isolated\n")
+
+		outf.write("color gray, isolated\n")
+
+		outf.write("\n")
+		outf.write("#clusters\n")
+		outf.write("\n")
+		outf.write("select red_clust, resi %s\n" % ("+".join(clusters[0])) )
+		outf.write("color red, red_clust\n")
+		outf.write("show spheres, red_clust\n")
+		outf.write("\n")
+		outf.write("select blue_clust, resi %s\n" % ("+".join(clusters[1])) )
+		outf.write("color blue, blue_clust\n")
+		outf.write("show spheres, blue_clust\n")
+		outf.write("\n")
+		outf.write("select green_clust, resi %s\n" % ("+".join(clusters[2])) )
+		outf.write("color green, green_clust\n")
+		outf.write("show spheres, green_clust\n")
+
+		for cluster in clusters[3:]:
+			outf.write("\n")
+			clust_id = "cluster_%d" % clusters.index(cluster)
+			outf.write("select %s, resi %s\n" % (clust_id, "+".join(cluster)) )
+			outf.write("color orange, %s\n" % clust_id)
+			outf.write("show spheres, %s\n" % clust_id)
+
+			outf.write("\ndeselect\n")
+
+
+		outf.close()
 
 	cursor.close()
 	db.close()
