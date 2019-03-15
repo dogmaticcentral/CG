a661f15be4c0a9187914510b2d7329a50782c1ba
ivana <ivana.mihalek@google.com>
2019-03-15 22:15:09 +0100

    copyright

--- icgc/config.py
+++ icgc/config.py
@@ -1,4 +1,22 @@
 
+#
+# This source code is part of icgc, an ICGC processing pipeline.
+# 
+# Icgc is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# Icgc is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see<http://www.gnu.org/licenses/>.
+# 
+# Contact: ivana.mihalek@gmail.com
+#
 class Config:
 	release  = "27" # this refers tio ICGC release number
 	data_home_local = "/storage/databases/icgc/v"+release
