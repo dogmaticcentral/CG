9c90e3729959d9ab4520e5b9da7dfca3d7322dbf
Ivana Mihalek <ivana.mihalek@gmail.com>
2019-05-31 21:20:45 +0200

    treebase

--- icgc/60_nextgen_production/62_reactome_tree.py
+++ icgc/60_nextgen_production/62_reactome_tree.py
@@ -17,7 +17,7 @@
 #
 # Contact: ivana.mihalek@gmail.com
 #
-
+from icgc_utils.common_queries import quotify
 from icgc_utils.mysql import  *
 from config import Config
 # https://networkx.github.io/documentation/stable/index.html
@@ -43,12 +43,32 @@ def main():
 	graph = nx.DiGraph(ret) # directed graph
 	print("graph is directed: ", graph.is_directed())
 	print("number of edges:", len(graph.edges))
-	print("graph is multigraph: ", graph.is_multigraph())
+	print("graph is multigraph:", graph.is_multigraph())
 	try:
 		edges = nx.find_cycle(graph)
 	except:
 		print("hooray, no cycles found")
-	
+
+	# graph.in_degree is a list of pairs, rather than a method
+
+	# candidate roots
+	zero_in_degee_nodes = [name for name, indegree in graph.in_degree if indegree==0]
+
+	node_id_string = ",".join([quotify(z) for z in zero_in_degee_nodes])
+	qry_template = "select * from reactome_pathways where reactome_pathway_id in (%s)"
+	root_names =  hard_landing_search(cursor, qry_template% node_id_string)
+	print("zero in-degree nodes:")
+	for pthwy_id, name in root_names:
+		print(pthwy_id, name)
+		# this is the whole subtree
+		# children = [node for node in nx.dfs_preorder_nodes(graph, pthwy_id)]
+		# A successor of n is a node m such that there exists a directed edge from n to m.
+		children = [node for node in graph.successors(pthwy_id)]
+		node_id_string = ",".join([quotify(z) for z in children])
+		children_names = hard_landing_search(cursor, qry_template% node_id_string)
+		for child_id, child_name in children_names:
+			print("\t", child_id, child_name)
+
 
 	cursor.close()
 	db.close()
