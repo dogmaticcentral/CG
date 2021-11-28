511862d7c12f9abd011cc11904908bcea7d599d6
saphetor <ivana.mihalek@saphetor.com>
2021-11-28 19:48:06 +0100

    hgnc name resolution table fix

--- icgc/10_local_db_loading/20_hgnc_name_resolution_table.py
+++ icgc/10_local_db_loading/20_hgnc_name_resolution_table.py
@@ -35,81 +35,79 @@
 from config import Config
 from icgc_utils.mysql import *
 
-header_names = ["hgnc_id", "approved_symbol", "approved_name",
-				"locus_group", "synonyms", "chromosome",
-				"ensembl_gene_id_by_hgnc", "refseq_ids", "uniprot_ids",
-				"ensembl_gene_id"]
 
 #########################################
-def make_hgnc_table(cursor, db_name, hgnc_table):
+def make_hgnc_table(cursor, db_name, hgnc_table, header_names):
 
-	if check_table_exists (cursor, db_name, hgnc_table): return
-	switch_to_db (cursor, db_name)
+    if check_table_exists (cursor, db_name, hgnc_table): return
+    switch_to_db (cursor, db_name)
 
-	qry = ""
-	qry += "  CREATE TABLE  %s (" % hgnc_table
-	qry += "     id INT NOT NULL, "
-	for name in header_names:
-		if name in ['approved_name', 'synonyms', 'refseq_ids']:
-			charlen = 150
-		elif name == 'uniprot_ids':
-			charlen = 300
-		elif name in ['approved_symbol', 'chromosome']: # chrom can have annotation such as "not on reference assembly"
-			charlen = 30
-		else:
-			charlen = 20
-		qry += " %s VARCHAR(%d)," % (name, charlen)
+    qry = ""
+    qry += "  CREATE TABLE  %s (" % hgnc_table
+    qry += "     id INT NOT NULL, "
+    for name in header_names:
+        if name in ['approved_name', 'synonyms', 'refseq_ids']:
+            charlen = 150
+        elif name == 'uniprot_ids':
+            charlen = 300
+        elif name in ['approved_symbol', 'chromosome']: # chrom can have annotation such as "not on reference assembly"
+            charlen = 30
+        else:
+            charlen = 20
+        qry += " %s VARCHAR(%d)," % (name, charlen)
 
-	qry += "	 PRIMARY KEY (id) "
-	qry += ") ENGINE=MyISAM"
+    qry += "	 PRIMARY KEY (id) "
+    qry += ") ENGINE=MyISAM"
 
-	rows = search_db(cursor, qry)
-	print(qry)
-	print(rows)
-	return
+    rows = search_db(cursor, qry)
+    print(qry)
+    print(rows)
+    return
 
 #########################################
 def strip_arm_annotation(chrom_address):
-	if "p" in chrom_address:
-		return chrom_address.split("p")[0]
-	if "q" in chrom_address:
-		return chrom_address.split("q")[0]
-	return chrom_address
+    if "p" in chrom_address:
+        return chrom_address.split("p")[0]
+    if "q" in chrom_address:
+        return chrom_address.split("q")[0]
+    return chrom_address
 
 #########################################
 #########################################
 def main():
 
-	hgncfile = "/storage/databases/hgnc/hgnc_name_res.tsv"
+    hgncfile = "/storage/databases/hgnc/hgnc_name_res.tsv"
 
-	tmp_outfile = "hgnctmp.tsv"
-	ct = 0
-	outf = open (tmp_outfile, "w")
-	with open(hgncfile, "r") as inf:
-		headers = inf.readline().rstrip("\n").split("\t")
-		chromosome_column = headers.index('Chromosome')
-		print(chromosome_column)
-		for line in inf:
-			fields = line.rstrip("\n").split("\t")
-			fields[chromosome_column] = strip_arm_annotation(fields[chromosome_column])
-			ct += 1
-			outfields = [str(ct)] + fields
-			outf.write("\t".join(outfields)+"\n")
-	outf.close()
+    tmp_outfile = "hgnctmp.tsv"
+    ct = 0
+    outf = open (tmp_outfile, "w")
+    with open(hgncfile, "r") as inf:
+        headers = inf.readline().rstrip("\n").split("\t")
+        chromosome_column = headers.index('Chromosome')
+        for line in inf:
+            fields = line.rstrip("\n").split("\t")
+            fields[chromosome_column] = strip_arm_annotation(fields[chromosome_column])
+            ct += 1
+            outfields = [str(ct)] + fields
+            outf.write("\t".join(outfields)+"\n")
+    outf.close()
 
-	db     = connect_to_mysql(Config.mysql_conf_file)
-	cursor = db.cursor()
+    db     = connect_to_mysql(Config.mysql_conf_file)
+    cursor = db.cursor()
 
-	db_name = "icgc"
+    qry = "SET GLOBAL local_infile=1"
+    error_intolerant_search(cursor, qry)
 
-	make_hgnc_table(cursor, db_name, "hgnc")
-	qry = "load data local infile '%s' into table %s" % ("hgnctmp.tsv","hgnc")
-	search_db(cursor,qry,verbose=True)
-	os.remove(tmp_outfile)
+    db_name = "identifier_maps"
+    new_headers = ["_".join(h.lower().replace("(supplied", "").replace(")", "").split(" ")) for h in headers]
+    make_hgnc_table(cursor, db_name, "hgnc", new_headers)
+    qry = "load data local infile '%s' into table %s" % ("hgnctmp.tsv","hgnc")
+    search_db(cursor,qry,verbose=True)
+    os.remove(tmp_outfile)
 
-	cursor.close()
-	db.close()
+    cursor.close()
+    db.close()
 
 #########################################
 if __name__ == '__main__':
-	main()
+    main()
